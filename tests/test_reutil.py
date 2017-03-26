7a479c7a3cbb229e945366a181f25c2cd1be9434
oleks <oleks@oleks.info>
2017-03-26 15:59:52 +0200

    Define your lambda functions

--- tests/test_reutil.py
+++ tests/test_reutil.py
@@ -3,12 +3,23 @@ import re
 
 from trafaret import reutil
 from trafaret.tokens import OffsetToken
-from typing import Pattern
+from typing import List, Match, Pattern
 
 
 @pytest.fixture(scope='session')
 def words() -> Pattern:
     return re.compile(r'(\W)')
+
+
+def text_transform(offset: int, text: str) -> List[OffsetToken]:
+    return [OffsetToken(offset, text)]
+
+
+def match_transform(m: Match) -> List[OffsetToken]:
+    if reutil.not_whitespace(m):
+        return text_transform(m.start(0), m.group(0))
+    else:
+        return []
 
 
 def test_reutil(words: Pattern) -> None:
@@ -21,7 +32,6 @@ def test_reutil(words: Pattern) -> None:
     text = " ".join(map(lambda t: t.text, expected))  # type: str
     actual = reutil.split_tokens(
         text, words,
-        lambda m: [m for m in [m] if reutil.not_whitespace(m)],
-        lambda i, t: [OffsetToken(i, t)])
+        match_transform, text_transform)
 
     assert actual == expected
