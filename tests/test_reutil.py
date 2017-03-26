8f6c0ec2ca58bdc1b9542738b3257f5cebe5f968
oleks <oleks@oleks.info>
2017-03-26 16:39:39 +0200

    Test both with whitespace and an operator

--- tests/test_reutil.py
+++ tests/test_reutil.py
@@ -3,7 +3,9 @@ import re
 
 from trafaret import reutil
 from trafaret.tokens import OffsetToken
-from typing import List, Match, Pattern
+from typing import Iterable, List, Match, Pattern, Tuple, TypeVar  # noqa: F401
+from itertools import repeat
+from functools import reduce
 
 
 @pytest.fixture(scope='session')
@@ -22,14 +24,53 @@ def match_transform(m: Match) -> List[OffsetToken]:
         return []
 
 
+A = TypeVar('A')
+
+
+# http://stackoverflow.com/a/5656097/5801152
+def intersperse(iterable: Iterable[A], delimiter: A) -> Iterable[A]:
+    it = iter(iterable)
+    yield next(it)
+    for x in it:
+        yield delimiter
+        yield x
+
+
+def flatten2(iterable: Iterable[Iterable[A]]) -> Iterable[A]:
+    return [i for subis in iterable for i in subis]
+
+
+def offset_tokens(
+        tokens: Iterable[str], offset: int = 0
+        ) -> Iterable[OffsetToken]:
+    init = (0, [])  # type: Tuple[int, List[OffsetToken]]
+    return reduce(
+        lambda x, y: (x[0] + len(y) + offset, x[1] + [OffsetToken(x[0], y)]),
+        tokens, init)[1]
+
+
 def test_whitespace_split(words: Pattern) -> None:
-    expected = [
-            OffsetToken(0, 'a'),
-            OffsetToken(2, 'b'),
-            OffsetToken(4, 'c'),
-            OffsetToken(6, 'd')
-        ]
-    text = " ".join(map(lambda t: t.text, expected))  # type: str
+    tokens = ['a', 'b', 'c', 'd']
+
+    delimeter = ' '
+    expected = offset_tokens(tokens, len(delimeter))
+
+    tokens = list(flatten2(zip(tokens, repeat(delimeter))))
+    text = ''.join(tokens)
+
+    actual = reutil.split_tokens(
+        text, words,
+        match_transform, text_transform)
+
+    assert actual == expected
+
+
+def test_operator_split(words: Pattern) -> None:
+    tokens = ['a', 'b', 'c', 'd']
+    tokens = list(flatten2(zip(tokens, repeat('+'))))
+    text = ''.join(tokens)
+
+    expected = offset_tokens(tokens)
     actual = reutil.split_tokens(
         text, words,
         match_transform, text_transform)
