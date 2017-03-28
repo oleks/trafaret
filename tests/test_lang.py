d4cd60b6c577b1df9fab73d6851046e6c3f44c4e
oleks <oleks@oleks.info>
2017-03-28 11:23:20 +0200

    Type annotation for the test function

--- tests/test_lang.py
+++ tests/test_lang.py
@@ -1,9 +1,7 @@
-import trafaret
-
 from trafaret.lang import CSharp
 
 
-def test_csharp_comment():
+def test_csharp_comment() -> None:
     lang = CSharp()
     comment = '// this is a comment \n'
     m = lang.comment.match(comment)
