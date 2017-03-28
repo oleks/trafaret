0fe2837d8edc967497186fab0eb3e713b292caaa
oleks <oleks@oleks.info>
2017-03-28 17:03:01 +0200

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     keywords=["trafaret", "programming", "exercises", "teaching"],
     long_description=readsybling('README.rst'),
     url="https://github.com/oleks/trafaret",
-    version='version=0.3.0',
+    version='version=0.4.0',
     packages=find_packages(),
     maintainer="Oleks",
     maintainer_email="oleks@oleks.info",
