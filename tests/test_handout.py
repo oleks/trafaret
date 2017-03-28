3c1f240e7548684edba384158500e8a06c5a3b53
oleks <oleks@oleks.info>
2017-03-28 10:58:29 +0200

    A test for the handout command

---
+++ tests/test_handout.py
@@ -0,0 +1,32 @@
+import os.path
+import subprocess
+import pytest
+
+from typing import List
+from _pytest.tmpdir import TempdirFactory
+
+
+@pytest.fixture(scope='session')
+def out_dirpath(tmpdir_factory: TempdirFactory) -> str:
+    return str(tmpdir_factory.mktemp('trafaret'))
+
+
+def output_to_file(command: List[str], path: str) -> None:
+    with open(path, 'w') as f:
+        f.write(subprocess.check_output(command).decode('utf-8'))
+
+
+def test_show(out_dirpath: str) -> None:
+    basename = "PythonArgmax"
+
+    yaml_path = os.path.join("exercises", basename + ".yaml")
+    data_dirpath = os.path.join("tests", "data")
+
+    md_name = basename + ".py"
+    md_path = os.path.join(data_dirpath, md_name)
+    out_path = os.path.join(out_dirpath, md_name)
+
+    command = ["trafaret", "handout", yaml_path]
+    output_to_file(command, out_path)
+
+    subprocess.check_call(["diff", "-ru", md_path, out_path])
